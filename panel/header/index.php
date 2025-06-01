ae0207c8bf2e50a915253180d68e1a88055ce372
amir <amirtaki623@gmail.com>
2025-06-01 21:45:19 +0330

    base html header index panelAdmin

---
+++ panel/header/index.php
@@ -0,0 +1,20 @@
+<?php
+    require_once "../../functions/check_section.php";
+    require_once "../../functions/helpers.php";
+    require_once "../../functions/pdo_connection.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= url('src/styles/panelAdimin.css')?>">
+    <title>Document</title>
+</head>
+<body>
+    
+    
+    <script src = "<?= rul('src/script/panel.js') ?>"></script>
+</body>
+</html>
