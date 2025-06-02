ac919c9493a5acb0b99e6b0d3487721989cb9b4f
amir <amirtaki623@gmail.com>
2025-06-02 17:59:39 +0330

    check status megaMenu_title panelAdmin

---
+++ panel/megaMenu_title/status.php
@@ -0,0 +1,11 @@
+<?php
+    require_once "../../functions/check_section.php";
+    require_once "../../functions/helpers.php";
+    require_once "../../functions/pdo_connection.php";
+
+    if(isset($_GET['id']) and $_GET['id'] !== ""){
+        $table = readTable ("adidas", "SELECT * FROM adidas.megaMenu_title WHERE id = ?", $single = true, $execute = [$_GET['id']]);
+        $status = $table->status == 10 ? 0 : 10; 
+        operationsDataBase ("adidas", "UPDATE adidas.megaMenu_title SET status = ? WHERE id = ?", $execute = [$status, $_GET['id']]);
+    }
+    redirect('panel/megaMenu_title');
