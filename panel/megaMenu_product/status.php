d1c8eba50527eb142f5d2f43607c93fec2d8cdc1
amir <amirtaki623@gmail.com>
2025-06-02 22:52:28 +0330

    status php megaMenu_product panelAdmin

---
+++ panel/megaMenu_product/status.php
@@ -0,0 +1,12 @@
+<?php
+    require_once "../../functions/check_section.php";
+    require_once "../../functions/helpers.php";
+    require_once "../../functions/pdo_connection.php";
+    if(isset($_GET['id']) && $_GET['id']!== ""){
+        $table = readTable ("adidas", "SELECT * FROM adidas.megamenu_product WHERE id = ?", $single = true, $execute = [$_GET['id']]);
+        if($table){
+            $status =  $table->status == 10 ? 0 : 10;
+            operationsDataBase ("adidas", "UPDATE adidas.megamenu_product SET status = ? WHERE id = ? ", $execute = [$status ,$_GET['id']]);
+        }
+    }
+    redirect('panel/megaMenu_product');
