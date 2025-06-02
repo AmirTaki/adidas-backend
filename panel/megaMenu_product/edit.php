8c6685991b9d58c4415d9c193a868d68cee3dcf6
amir <amirtaki623@gmail.com>
2025-06-02 22:52:48 +0330

    status php megaMenu_product panelAdmin

---
+++ panel/megaMenu_product/edit.php
@@ -0,0 +1,21 @@
+<?php
+    require_once "../../functions/check_section.php";
+    require_once "../../functions/helpers.php";
+    require_once "../../functions/pdo_connection.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= url('src/styles/panelAdimin.css')?>">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <?php require_once '../layouts/navbar.php' ?>
+    <?php require_once '../layouts/sidebar.php' ?>
+    <script src = "<?= url('src/script/panel.js') ?>"></script>
+</body>
+</html>
