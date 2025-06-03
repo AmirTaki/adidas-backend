c7c32f3ca0efc13ea0c84dcb609458c994f1ca4a
amir <amirtaki623@gmail.com>
2025-06-03 21:31:26 +0330

    delete.php scroller image panel Admin

---
+++ panel/scrollerImg_Main/delete.php
@@ -0,0 +1,12 @@
+<?php
+    require_once "../../functions/check_section.php";
+    require_once "../../functions/helpers.php";
+    require_once "../../functions/pdo_connection.php";
+    if(isset($_GET['id']) && $_GET['id'] !== ""){
+        $table = readTable ("adidas", "SELECT * FROM adidas.scrollerimg_main WHERE id = ?", $single = true, $execute = [$_GET['id']]);
+        if($table){
+            deleteFiles($table->path);
+            operationsDataBase ('adidas', "DELETE FROM adidas.scrollerimg_main WHERE id = ? ", $execute = [$_GET['id']]);
+        }
+    }
+    redirect("panel/scrollerImg_Main");
