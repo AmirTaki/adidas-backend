ba6bd2e33c276e75d41aaedca456b77f1068941a
amir <amirtaki623@gmail.com>
2025-06-03 19:47:06 +0330

    part 1 delete php advert_main panelAdmin

---
+++ panel/advert_main/delete.php
@@ -0,0 +1,10 @@
+<?php
+    require_once "../../functions/check_section.php";
+    require_once "../../functions/helpers.php";
+    require_once "../../functions/pdo_connection.php";
+    if(isset($_GET['id']) && $_GET['id'] !== ""){
+        $table = readTable ("adidas", "SELECT * FROM adidas.advert_main WHERE id = ?", $single = true, $execute = [$_GET['id']]);
+        if($table){
+            
+        }
+    }
