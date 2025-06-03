f0bd58bcf9b942e342a8b2fa06c00c775224385b
amir <amirtaki623@gmail.com>
2025-06-03 20:00:44 +0330

    part 2 delete php advert_main panelAdmin

--- panel/advert_main/delete.php
+++ panel/advert_main/delete.php
@@ -5,6 +5,8 @@
     if(isset($_GET['id']) && $_GET['id'] !== ""){
         $table = readTable ("adidas", "SELECT * FROM adidas.advert_main WHERE id = ?", $single = true, $execute = [$_GET['id']]);
         if($table){
-            
+            deleteFiles($table->path);
+            operationsDataBase ('adidas', "DELETE FROM adidas.advert_main WHERE id = ? ", $execute = [$_GET['id']]);
         }
     }
+    redirect("panel/advert_main");
