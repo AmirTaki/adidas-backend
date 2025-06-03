21c38e72723cb78563bc337956d25e5cb304ad6c
amir <amirtaki623@gmail.com>
2025-06-03 20:06:42 +0330

    part 1  edit.php advert_main panelAdmin

--- panel/advert_main/edit.php
+++ panel/advert_main/edit.php
@@ -16,6 +16,28 @@
 <body>
     <?php require_once '../layouts/navbar.php' ?>
     <?php require_once '../layouts/sidebar.php' ?>
+    <div style = 'padding-top:150px'>
+        <form action="<?= url('panel/advert_main/edit.php?id='.$_GET['id']); ?>" method="post" class = 'form_create_category' enctype="multipart/form-data">
+            
+            <!-- title  -->
+            <label for="name" >Title</label>
+            <input class = '' type="text" name="title" id="nameid" value  = '<?= $table->title ?>'>
+            <!-- image -->
+            <img src="<?= $table->path ?>" style = 'margin:25px;width:15%' alt="picture">
+            <!-- input image -->
+            <label for="" >Image</label>
+            <input  type = 'file' name = 'image' class = 'input_create_category'>
+            <!-- price  -->
+            <label for="name" >Price</label>
+            <input class = '' type="text" name="price" id="nameid" placeholder = 'enter the price ...' value = "<?= $table->price ?>">
+            <!-- body -->
+            <label for="" >Body</label>
+            <textarea name = 'body' placeholder = 'Enter the caption  ..... '><?= $table->body ?></textarea>
+            <!-- submit -->
+            <input type="submit" value="Create" class = 'sing_button'>
+            
+        </form>
+    </div>
 
     <script src = "<?= url('src/script/panel.js') ?>"></script>
 </body>
