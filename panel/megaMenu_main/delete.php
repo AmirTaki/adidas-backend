b960bfbfd76f74302d089c459d3620002b34f609
amir <amirtaki623@gmail.com>
2025-06-04 21:50:51 +0330

    deltee.php  megaMenu_main at panelAdmin

---
+++ panel/megaMenu_main/delete.php
@@ -0,0 +1,9 @@
+<?php 
+    require_once "../../functions/check_section.php";
+    require_once "../../functions/helpers.php";
+    require_once "../../functions/pdo_connection.php";
+
+    if(isset($_GET['id']) and $_GET['id']!== ""){
+        operationsDataBase ("adidas", "DELETE FROM adidas.megamenu_main WHERE id = ?", $execute = [$_GET["id"]]);
+    }
+    redirect("panel/megaMenu_main");
