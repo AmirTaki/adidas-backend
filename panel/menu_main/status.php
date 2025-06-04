20444af5a84292990706c212fa64d725976cc8e5
amir <amirtaki623@gmail.com>
2025-06-04 21:25:54 +0330

    status.php menu_main panelAdmin

---
+++ panel/menu_main/status.php
@@ -0,0 +1,14 @@
+<?php     
+    require_once "../../functions/check_section.php";
+    require_once "../../functions/helpers.php";
+    require_once "../../functions/pdo_connection.php";
+    
+    if(isset($_GET['id']) and $_GET['id']!== ""){
+       $table =  readTable ("adidas", "SELECT * FROM adidas.menu_main WHERE id = ?", $single = true, $execute = [$_GET['id']]);
+    
+       if($table){
+            $status =  $table->status == 10 ? 0 : 10;
+            operationsDataBase ('adidas', "UPDATE adidas.menu_main SET status = ? WHERE id = ?", $execute = [$status, $_GET['id']]);
+        }
+    }
+    redirect('panel/menu_main');
