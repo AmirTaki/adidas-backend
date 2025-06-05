8d0252965694a5278570aacac0ebed3d05d67d25
amir <amirtaki623@gmail.com>
2025-06-05 21:15:03 +0330

    add teablesDataBase at function at porject

---
+++ functions/tablesDataBase.php
@@ -0,0 +1,29 @@
+<?php 
+    // header
+    $tableHeader = readTable ('adidas', "SELECT * FROM adidas.header", $single = false, $execute = null);
+    // megaMenu_title
+    $TableMegaMenuTitle = readTable ("adidas", "SELECT * FROM adidas.megamenu_title", $single = false, $execute = null);
+    // megaMenu_product
+    $TableMegaMenuProduct = readTable ("adidas", "SELECT * FROM adidas.megamenu_product", $single = false, $execute = null);
+    // advert_main
+    $TableAdvertMain = readTable ("adidas", "SELECT * FROM adidas.advert_main", $single = false, $execute = null);
+    // scroller_image_main
+    $TableScrollerImage =  readTable ("adidas", "SELECT * FROM adidas.scrollerimg_main", $single = false, $execute = null);
+    // scroller_video
+    $TableScrollerVideo = readTable ("adidas", "SELECT * FROM adidas.scroller_video", $single = false, $execute = null);
+    // popular_title
+    $TablePopularTitle = readTable ("adidas", "SELECT * FROM adidas.popular_title", $single = false, $execute = null);
+    // related_resources
+    $TableRelatedResources = readTable ('adidas', "SELECT * FROM adidas.related_resources", $single = false, $execute = null);
+    // menu_main
+    $TableMenuMain = readTable ("adidas", "SELECT * FROM adidas.menu_main", $single = false, $execute = null);
+    // megaMenu_main
+    $TableMegaMenuMain = readTable ("adidas", "SELECT * FROM adidas.megamenu_main", $single = false, $execute = null);
+    // board_black 
+    $TableBoardBlack = readTable ("adidas", "SELECT * FROM adidas.board_black", $single = false, $execute = null);
+    // menu_footer
+    $TableMenuFooer = readTable ("adidas", "SELECT * FROM adidas.menu_footer", $single = false, $execute = null);
+    // megeMenu_footer
+    $TableMegaMenuFooter = readTable ("adidas", "SELECT * FROM adidas.megamenu_footer", $single = false, $execute = null);
+    // footer_column
+    $TableFooterColumn = readTable ("adidas", "SELECT * FROM adidas.footer_column", $single = false, $execute = null);
